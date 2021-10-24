131459992510bd34b8e0b4be2c69a3976d9d4bda
katemcg93 <katemcg93@gmail.com>
2021-10-24 14:58:34 +0100

    heatmap

--- matplotlib_plots.py
+++ matplotlib_plots.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import csv
+import matplotlib.cm as cmap
 
 pd.set_option("display.precision", 2)
 
@@ -10,8 +11,6 @@ fh_dataframe = pd.read_csv('C:\\Users\\Owner\\Desktop\\Fundamentals-of-Data-Anal
 description = fh_dataframe.describe()
 print(description)
 
-for col in fh_dataframe.columns:
-    print(col)
 
 fh_dataframe = fh_dataframe.replace({'male':1}, value = 'Male')
 fh_dataframe = fh_dataframe.replace({'male':0}, value = 'Female')
@@ -27,4 +26,21 @@ plt.hist(eduLevel2["BMI"], stacked = True, label = 'Education Level 2', color = 
 plt.hist(eduLevel3["BMI"], stacked = True, label = 'Education Level 3', color = '#EAD94C')
 plt.hist(eduLevel4["BMI"], stacked = True, label = 'Education Level 4', color = '#51A3A3')
 legend = plt.legend()
+plt.show()
+plt.close()
+
+
+corr_coeff_fh = fh_dataframe.corr()
+print(corr_coeff_fh)
+fig = plt.figure(figsize = (20,20))
+ax = fig.add_subplot(111)
+corrmat = ax.matshow(corr_coeff_fh,cmap = 'coolwarm', vmin = -1, vmax = 1)
+fig.colorbar(corrmat)
+ticks = np.arange(0, len(corr_coeff_fh.columns), 1)
+print(ticks)
+ax.set_xticks(ticks)
+plt.xticks(rotation=90)
+ax.set_yticks(ticks)
+ax.set_xticklabels(corr_coeff_fh.columns)
+ax.set_yticklabels(corr_coeff_fh.columns)
 plt.show()
