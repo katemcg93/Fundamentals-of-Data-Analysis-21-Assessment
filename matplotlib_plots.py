a8ce63631dbfd9e4e7db47531ac43653fe1847d5
katemcg93 <katemcg93@gmail.com>
2021-12-18 17:19:11 +0000

    stack plot

--- matplotlib_plots.py
+++ matplotlib_plots.py
@@ -1,8 +1,10 @@
+import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import csv
 import matplotlib.cm as cmap
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter
 
 pd.set_option("display.precision", 2)
 
@@ -42,4 +44,58 @@ ax.set_yticks(ticks)
 ax.set_xticklabels(corr_coeff_fh.columns)
 ax.set_yticklabels(corr_coeff_fh.columns)
 plt.show()
+plt.close()
 
+#Reading csv containing data from Eurostat on Weekly Covid Cases
+covid_df = pd.read_csv("Covid Case Data Eurostat.csv")
+
+#File has cases and deaths, isolating data on cases to plot
+covid_df = covid_df.loc[covid_df["indicator"] == "cases"]
+
+#Creating new column with numeric form of year - week to filter on last 4 weeks
+covid_df["Year_Week_int"] = covid_df["year_week"].str.replace("-", "")
+covid_df["Year_Week_int"] = pd.to_numeric(covid_df["Year_Week_int"])
+covid_4_wks = covid_df.loc[covid_df["Year_Week_int"] > 202145]
+
+#Isolating the total weekly case numbers for each continent into its own dataframe to plot later
+covid_africa = covid_4_wks.loc[covid_4_wks["country"] == "Africa (total)"]
+covid_europe = covid_4_wks.loc[covid_4_wks["country"] == "Europe (total)"]
+covid_america = covid_4_wks.loc[covid_4_wks["country"] == "America (total)"]
+covid_asia = covid_4_wks.loc[covid_4_wks["country"] == "Asia (total)"]
+covid_oceania= covid_4_wks.loc[covid_4_wks["country"] == "Oceania (total)"]
+
+#Retrieving weekly count figures for each continent
+y1 = covid_africa["weekly_count"]
+y2 = covid_europe ["weekly_count"]
+y3 = covid_america ["weekly_count"]
+y4 = covid_asia ["weekly_count"]
+y5 = covid_oceania["weekly_count"]
+
+#Setting up x axis - bins and tick labels
+x = [0,1,2,3]
+tick_labels = ["Wk 46", "Wk 47", "Wk 48", "Wk 49"]
+
+#Labels for legent
+labels = ["Africa", "Europe", "America", "Asia", "Oceania"]
+
+#Converting the pandas columns with weekly case numbers to numpy arrays
+array1 = y1.to_numpy()
+array2 = y2.to_numpy()
+array3 = y3.to_numpy()
+array4 = y4.to_numpy()
+array5 = y5.to_numpy()
+
+
+fig, ax = plt.subplots()
+ax.stackplot(x, array1, array2, array3, array4, array5, labels = labels)
+ax.locator_params(tight=True, nbins=4)
+ax.set_xticks(range(len(tick_labels)))
+ax.set_xticklabels(labels = tick_labels)
+
+ax.set_xlabel("Week Number")
+ax.set_ylabel("Weekly Covid Cases in Millions")
+
+#Adding a tick label at each 1m interval
+ax.set_yticks(np.arange(0, 5000000, 1000000))
+plt.legend()
+plt.show()
